5b286e2d31ea4d84d650351e4e8cc1d16901a6f9
Yaser Moradi <ysmoradi@outlook.com>
2021-01-09 00:56:00 +0330

    lamar sample

--- ServerTests.cs
+++ ServerTests.cs
@@ -1,6 +1,4 @@
-using Autofac;
-using Autofac.Extensions.DependencyInjection;
-using Microsoft.AspNetCore;
+using Lamar;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -8,8 +6,6 @@ using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace AspIntegrationTestMemoryLeakIssue
@@ -19,35 +15,10 @@ namespace AspIntegrationTestMemoryLeakIssue
     {
         [DataTestMethod]
         [DataRow, DataRow, DataRow]
-        public async Task RealServerTest() // no issue! (See Additional context at bottom of issue)
-        {
-            using var webHost = WebHost.CreateDefaultBuilder()
-                .UseUrls("http://localhost/")
-                .UseStartup<Startup1>()
-                .Build();
-
-            await webHost.StartAsync();
-
-            using HttpClient client = new();
-
-            Assert.AreEqual("Hello World!", (await (await client.GetAsync("http://localhost/")).EnsureSuccessStatusCode().Content.ReadAsStringAsync()));
-        }
-
-        [DataTestMethod]
-        [DataRow, DataRow, DataRow]
-        public async Task TestServerTest_WebHost()
-        {
-            using TestServer server = new(WebHost.CreateDefaultBuilder().UseUrls("http://localhost/").UseStartup<Startup1>());
-
-            Assert.AreEqual("Hello World!", (await (await server.CreateClient().GetAsync("/")).EnsureSuccessStatusCode().Content.ReadAsStringAsync()));
-        }
-
-        [DataTestMethod]
-        [DataRow, DataRow, DataRow]
         public async Task TestServerTest_GenericHost()
         {
             using IHost host = new HostBuilder()
-                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+                .UseServiceProviderFactory<ServiceRegistry>(new LamarServiceProviderFactory())
                 .ConfigureWebHostDefaults(webHostBuilder =>
                 {
                     webHostBuilder
@@ -65,36 +36,9 @@ namespace AspIntegrationTestMemoryLeakIssue
         }
     }
 
-    public class Startup1 // web host builder
-    {
-        public virtual IServiceProvider ConfigureServices(IServiceCollection services)
-        {
-            ContainerBuilder builder = new ContainerBuilder();
-
-            builder.Populate(services);
-
-            IContainer container = builder.Build();
-
-            return new AutofacServiceProvider(container);
-        }
-
-        public void Configure(IApplicationBuilder app)
-        {
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
-        }
-    }
-
     public class Startup2 // generic host
     {
-        public void ConfigureContainer(ContainerBuilder builder)
+        public void ConfigureContainer(ServiceRegistry services)
         {
 
         }
