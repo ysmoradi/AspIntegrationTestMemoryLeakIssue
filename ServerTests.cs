d79ff755dfe630d0ac1b3115d5d6e868c71f282d
Yaser Moradi <ysmoradi@outlook.com>
2021-01-09 01:11:33 +0330

    added ref count assertion

--- ServerTests.cs
+++ ServerTests.cs
@@ -9,6 +9,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -31,6 +33,10 @@ namespace AspIntegrationTestMemoryLeakIssue
             using HttpClient client = new();
 
             Assert.AreEqual("Hello World!", (await (await client.GetAsync("http://localhost/")).EnsureSuccessStatusCode().Content.ReadAsStringAsync()));
+
+            GC.Collect(GC.MaxGeneration, GCCollectionMode.Forced);
+
+            Assert.AreEqual(1, GCContainer.References.Count(r => r.IsAlive));
         }
 
         [DataTestMethod]
@@ -40,6 +46,10 @@ namespace AspIntegrationTestMemoryLeakIssue
             using TestServer server = new(WebHost.CreateDefaultBuilder().UseUrls("http://localhost/").UseStartup<Startup1>());
 
             Assert.AreEqual("Hello World!", (await (await server.CreateClient().GetAsync("/")).EnsureSuccessStatusCode().Content.ReadAsStringAsync()));
+
+            GC.Collect(GC.MaxGeneration, GCCollectionMode.Forced);
+
+            Assert.AreEqual(1, GCContainer.References.Count(r => r.IsAlive));
         }
 
         [DataTestMethod]
@@ -62,11 +72,26 @@ namespace AspIntegrationTestMemoryLeakIssue
             using TestServer server = host.GetTestServer();
 
             Assert.AreEqual("Hello World!", (await (await server.CreateClient().GetAsync("/")).EnsureSuccessStatusCode().Content.ReadAsStringAsync()));
+
+            GC.Collect(GC.MaxGeneration, GCCollectionMode.Forced);
+
+            Assert.AreEqual(1, GCContainer.References.Count(r => r.IsAlive));
         }
     }
 
+    public static class GCContainer
+    {
+        public static List<WeakReference> References { get; set; } = new List<WeakReference>();
+    }
+
+
     public class Startup1 // web host builder
     {
+        public Startup1()
+        {
+            GCContainer.References.Add(new WeakReference(this));
+        }
+
         public virtual IServiceProvider ConfigureServices(IServiceCollection services)
         {
             ContainerBuilder builder = new ContainerBuilder();
@@ -94,6 +119,11 @@ namespace AspIntegrationTestMemoryLeakIssue
 
     public class Startup2 // generic host
     {
+        public Startup2()
+        {
+            GCContainer.References.Add(new WeakReference(this));
+        }
+
         public void ConfigureContainer(ContainerBuilder builder)
         {
 
