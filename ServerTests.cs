b839b5f93974837596c19a774fac8f8d3fa00cf1
Yaser Moradi <ysmoradi@outlook.com>
2021-01-09 01:06:25 +0330

    added ref count assertion

--- ServerTests.cs
+++ ServerTests.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace AspIntegrationTestMemoryLeakIssue
@@ -22,11 +24,25 @@ namespace AspIntegrationTestMemoryLeakIssue
             using TestServer server = new(WebHost.CreateDefaultBuilder().UseUrls("http://localhost/").UseStartup<Startup1>());
 
             Assert.AreEqual("Hello World!", (await (await server.CreateClient().GetAsync("/")).EnsureSuccessStatusCode().Content.ReadAsStringAsync()));
+
+            GC.Collect(GC.MaxGeneration, GCCollectionMode.Forced);
+
+            Assert.AreEqual(1, GCContainer.References.Count(r => r.IsAlive));
         }
+    }
+
+    public static class GCContainer
+    {
+        public static List<WeakReference> References { get; set; } = new List<WeakReference>();
     }
 
     public class Startup1 // web host builder
     {
+        public Startup1()
+        {
+            GCContainer.References.Add(new WeakReference(this));
+        }
+
         public virtual IServiceProvider ConfigureServices(IServiceCollection services)
         {
             return WindsorRegistrationHelper.CreateServiceProvider(new WindsorContainer(), services);
