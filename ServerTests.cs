80e8425462125c432d68c48d531b0e7c12c223ec
Yaser Moradi <ysmoradi@outlook.com>
2021-01-09 00:48:52 +0330

    windsor sample

--- ServerTests.cs
+++ ServerTests.cs
@@ -1,15 +1,13 @@
-using Autofac;
-using Autofac.Extensions.DependencyInjection;
+using Castle.Windsor;
+using Castle.Windsor.MsDependencyInjection;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace AspIntegrationTestMemoryLeakIssue
@@ -19,47 +17,9 @@ namespace AspIntegrationTestMemoryLeakIssue
     {
         [DataTestMethod]
         [DataRow, DataRow, DataRow]
-        public async Task RealServerTest() // no issue! (See Additional context at bottom of issue)
-        {
-            using var webHost = WebHost.CreateDefaultBuilder()
-                .UseUrls("http://localhost/")
-                .UseStartup<Startup1>()
-                .Build();
-
-            await webHost.StartAsync();
-
-            using HttpClient client = new();
-
-            Assert.AreEqual("Hello World!", (await (await client.GetAsync("http://localhost/")).EnsureSuccessStatusCode().Content.ReadAsStringAsync()));
-        }
-
-        [DataTestMethod]
-        [DataRow, DataRow, DataRow]
         public async Task TestServerTest_WebHost()
         {
             using TestServer server = new(WebHost.CreateDefaultBuilder().UseUrls("http://localhost/").UseStartup<Startup1>());
-
-            Assert.AreEqual("Hello World!", (await (await server.CreateClient().GetAsync("/")).EnsureSuccessStatusCode().Content.ReadAsStringAsync()));
-        }
-
-        [DataTestMethod]
-        [DataRow, DataRow, DataRow]
-        public async Task TestServerTest_GenericHost()
-        {
-            using IHost host = new HostBuilder()
-                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
-                .ConfigureWebHostDefaults(webHostBuilder =>
-                {
-                    webHostBuilder
-                        .UseTestServer()
-                        .UseUrls("http://localhost/")
-                        .UseStartup<Startup2>();
-                })
-                .Build();
-
-            await host.StartAsync();
-
-            using TestServer server = host.GetTestServer();
 
             Assert.AreEqual("Hello World!", (await (await server.CreateClient().GetAsync("/")).EnsureSuccessStatusCode().Content.ReadAsStringAsync()));
         }
@@ -69,39 +29,7 @@ namespace AspIntegrationTestMemoryLeakIssue
     {
         public virtual IServiceProvider ConfigureServices(IServiceCollection services)
         {
-            ContainerBuilder builder = new ContainerBuilder();
-
-            builder.Populate(services);
-
-            IContainer container = builder.Build();
-
-            return new AutofacServiceProvider(container);
-        }
-
-        public void Configure(IApplicationBuilder app)
-        {
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
-        }
-    }
-
-    public class Startup2 // generic host
-    {
-        public void ConfigureContainer(ContainerBuilder builder)
-        {
-
-        }
-
-        public virtual void ConfigureServices(IServiceCollection services)
-        {
-
+            return WindsorRegistrationHelper.CreateServiceProvider(new WindsorContainer(), services);
         }
 
         public void Configure(IApplicationBuilder app)
